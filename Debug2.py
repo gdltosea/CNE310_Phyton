1e546f2b9feec7b1455e089a9b71ed2bd5e502a6
GnosisGrain <103087417+GnosisGrain@users.noreply.github.com>
2022-05-27 13:06:56 -0700

    Merge pull request #2 from gdltosea/Debug2Retry
    Update Debug2.py

--- Debug2.py
+++ Debug2.py
@@ -1,37 +1,22 @@
-@@ -1,27 +1,21 @@
-def score_to_letter_grade(grade):
-    if grade > 90:
+def score_to_letter_grade(grade):   #this function return a grade according to score 
+    if grade >= 90: #if score in greater or equals to 90 return A
         return "A"
-    elif grade >= 87:
+    elif grade >= 87:   #if score in greater or equals to 87 return B+
         return "B+"
-    elif grade == 80:
+    elif grade >= 80:   #if score in greater or equals to 80 return B
         return "B"
     elif grade >= 77:
         return "C+"
-    elif grade <= 70:
+    elif grade >= 70:
         return "C"
     elif grade >= 67:
         return "D+"
     elif grade >= 60:
         return "D"
-#function given in the question
-def lone_sum(a, b, c):
-    #sum contain the sum of 3 values in a,b and c
-    sum = a + b + c
-    if a >= b:
-        return c
-    elif a == c:
-        return b
-    elif b == c:
-        return a
-    elif a == b and a == c and b == c:
-        return 0
     else:
-        return "F"
-        return sum
+        return "F"  #otherwise return F 
 
-
-print("Grade of 90 should be A: " + score_to_letter_grade(90))
+print("Grade of 90 should be A: " + score_to_letter_grade(90))  #call score_to_letter_grade function and print output 
 print("Grade of 87 should be B+: " + score_to_letter_grade(87))
 print("Grade of 81 should be B: " + score_to_letter_grade(81))
 print("Grade of 77 should be C+: " + score_to_letter_grade(77))
@@ -39,9 +24,3 @@ print("Grade of 70 should be C: " + score_to_letter_grade(70))
 print("Grade of 67 should be D+: " + score_to_letter_grade(67))
 print("Grade of 60 should be D: " + score_to_letter_grade(60))
 print("Grade of 59 should be F: " + score_to_letter_grade(59))
-#input the values of a, b and c from the user
-a=int(input("Enter the value of a :"))
-b=int(input("Enter the value of b :"))
-c=int(input("Enter the value of c :"))
-#function call
-print(lone_sum(a,b,c) )
